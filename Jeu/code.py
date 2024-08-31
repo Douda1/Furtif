f55cddb685e582f1887fbc7bccba1850f87901c4
Douda1 <160403454+Douda1@users.noreply.github.com>
2024-08-31 13:50:39 +0400

    Debut

---
+++ Jeu/code.py
@@ -0,0 +1,27 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((1920,1017),
+                                 pygame.RESIZABLE)
+
+clock = pygame.time.Clock()
+
+while True:
+    # Process player inputs.
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            raise SystemExit
+
+    # Do logical updates here.
+    # ...
+    print(screen)
+
+    screen.fill("purple")  # Fill the display with a solid color
+
+    # Render the graphics here.
+    # ...
+
+    pygame.display.flip()  # Refresh on-screen display
+    clock.tick(60)         # wait until next frame (at 60 FPS)
