127eca08636157b80c56977d5cfe95efa986fd18
Douda1 <160403454+Douda1@users.noreply.github.com>
2024-08-31 14:24:45 +0400

    Furtivité

--- Jeu/code.py
+++ Jeu/code.py
@@ -7,19 +7,25 @@ screen = pygame.display.set_mode((1920,1017),
 
 clock = pygame.time.Clock()
 
+icone = pygame.image.load('Graphique/logo.png')
+
+pygame.display.set_icon(icone)
+
+pygame.display.set_caption("Histoire.odt — LibreOffice Writer")
+
 while True:
     # Process player inputs.
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             raise SystemExit
-
+    Background = pygame.image.load('Graphique/Background.png')
     # Do logical updates here.
     # ...
     print(screen)
 
     screen.fill("purple")  # Fill the display with a solid color
-
+    screen.blit(Background,(0,0))
     # Render the graphics here.
     # ...
 
