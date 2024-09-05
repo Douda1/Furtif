5609c2ef225f53c37b22a857af52416808114f5e
Douda1 <160403454+Douda1@users.noreply.github.com>
2024-09-05 12:54:23 +0400

    oui

--- Jeu/code_1.py
+++ Jeu/code_1.py
@@ -1,5 +1,6 @@
 import pygame
 import time
+import random
 
 pygame.init()
 
@@ -8,21 +9,24 @@ screen = pygame.display.set_mode((1920,1017),
 
 clock = pygame.time.Clock()
 
-icone = pygame.image.load('Graphique/logo.png')
+icone = pygame.image.load('Graphique\logo.png')
 
 pygame.display.set_icon(icone)
 
 pygame.display.set_caption("Histoire.odt — LibreOffice Writer")
 
+myfont = pygame.font.SysFont("monospace", 15)
 
-
+Score = 0
 AvionX = 200
 AvionY = 200
 ImageAvion = pygame.image.load('Graphique/logo.png')
 MissileX = AvionX
 MissileY = AvionY
 MissileRect = pygame.Rect(MissileX,MissileY,5,10)
-
+enemyx = 200
+enemyy = 200
+enemyRect = pygame.Rect(enemyx,enemyy,50,100)
 
 Activé = False
 
@@ -47,8 +51,8 @@ while True:
     screen.blit(Background,(0,0))
     
     AvionRect = pygame.Rect(AvionX,AvionY,10,10)
-    MissileRect = pygame.Rect(MissileX,MissileY,5,10)
-    
+    MissileRect = pygame.Rect(MissileX,MissileY,10,10)
+    enemyRect = pygame.Rect(enemyx,enemyy,30,50)
     
     if event.type == pygame.KEYDOWN:
         time.sleep(0.5)
@@ -70,15 +74,32 @@ while True:
     
     
     if Activé:
-        MissileY -= 10
-        if MissileY ==  100:
+        MissileY -= 20
+        if MissileY <  100:
             MissileY = AvionY
             MissileX = AvionX
         pygame.draw.rect(screen,(255,0,255), AvionRect)
         pygame.draw.rect(screen,(0,255,0), MissileRect)
+        pygame.draw.rect(screen,(255,0,0), enemyRect)
         
+    if Activé:
+        enemyy += 8
+        if enemyy > 1000:
+            enemyy = 100
+            Score = 0
+    if pygame.Rect.colliderect(enemyRect,MissileRect):
+        Score += 1
+        enemyy = 100
+        enemyx = random.randint(100, 500)
+    if Activé:
+        theScore = myfont.render(str(Score) , 1, (255,255,255))
+        LeScore = myfont.render("Score : " , 1, (255,255,255))
+        screen.blit(theScore, (125, 150))
+        screen.blit(LeScore, (40, 150))
 
-    print(AvionX)
+
+
+
     
     
     
